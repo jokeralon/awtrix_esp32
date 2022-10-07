5b3005446b3124d141aa95ffbad89bc000a0593e
alon <alon210121656@gmail.com>
2022-10-07 14:04:40 +0800

    fft 音乐频谱测试

--- project/awtrix/awtrix.c
+++ project/awtrix/awtrix.c
@@ -1,6 +1,8 @@
 #include "awtrix.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include "driver/rmt.h"
 #include "esp_system.h"
 #include "esp_log.h"
@@ -13,12 +15,14 @@
 #include <sys/time.h>
 
 #include "driver/adc.h"
+#include "driver/i2s.h"
 #include "esp_adc_cal.h"
+#include "esp_dsp.h"
 
 #include "awtrix_api.h"
 #include "awtrix_weather.h"
 #include "ws2812.h"
-#include "fft.h"
+#include "arduinoFFT.h"
 #include "adc.h"
 
 static const char *TAG = "awtrix";
@@ -28,7 +32,7 @@ static const char *TAG = "awtrix";
 #define EXAMPLE_ESP_WIFI_SSID "ChinaNet-BVBi"
 #define EXAMPLE_ESP_WIFI_PASS "e6s7tmuq"
 
-#define RMT_TX_NUM 0				 //发送口
+#define RMT_TX_NUM 2				 //发送口
 #define RMT_TX_CHANNEL RMT_CHANNEL_0 //发送频道
 #define LED_STRIP_NUM 257			 //灯珠数量
 
@@ -39,7 +43,6 @@ int awtrix_status_flag = 0;
 TaskHandle_t awtrix_display_handle = NULL;
 TaskHandle_t awtrix_status_handle = NULL;
 TaskHandle_t awtrix_weather_handle = NULL;
-TaskHandle_t awtrix_ferquency_handle = NULL;
 
 time_t now;
 struct tm timeinfo;
@@ -48,6 +51,21 @@ static pixel_u pixel[AWTRIX_MAX_RAW][AWTRIX_MAX_COL];
 static weather_t weather;
 
 awtrix_t awtrix;
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+	const long run = in_max - in_min;
+	if (run == 0)
+	{
+		ESP_LOGE(TAG, "map(): Invalid input range, min == max");
+		return -1; // AVR returns -1, SAM returns 0
+	}
+	const long rise = out_max - out_min;
+	const long delta = x - in_min;
+	return (delta * rise) / run + out_min;
+}
+
+
 
 void init_led()
 {
@@ -82,7 +100,7 @@ void awtrix_display_task(void *pvParameters) //任务函数
 	while (1)
 	{
 		if (pixel == NULL)
-			break;
+			continue;
 
 		uint8_t flag = 0;
 		int num = 0;
@@ -164,28 +182,75 @@ void awtrix_weather_task(void *pvParameters) //任务函数
 		vTaskDelay(pdMS_TO_TICKS(10 * 60 * 1000));
 	}
 }
-void awtrix_ferquency_task(void *pvParameters) //任务函数
-{
-	int adc_raw[32];
-	double pr[32], pi[32], fr[32], fi[32], t[32];
-	adc_init();
-	while (1)
-	{
-		for (int i = 0; i < 32; i++)
-		{
-			adc_raw[i] = adc_get_raw();
-			pi[i] = 0;
-			t[i] = i*0.001;
-			pr[i] = (float)adc_raw[i] ;
-		}
-		kfft(pr, pi, 32, 6, fr, fi);
-		for (int i = 0; i < 32; i++)
-		{
-			printf("%d\t%lf\n", i, pr[i]); //输出结果
-		}
 
-		vTaskDelay(pdMS_TO_TICKS(10000));
+#define SAMPLE_RATE (16000)
+#define I2S_NUM (0)
+#define WAVE_FREQ_HZ (100)
+#define PI (3.14159265)
+#define I2S_BCK_IO (GPIO_NUM_20)
+#define I2S_WS_IO (GPIO_NUM_21)
+#define I2S_DO_IO (-1)
+#define I2S_DI_IO (GPIO_NUM_10)
+
+void awtrix_i2s_init()
+{
+	i2s_config_t i2s_config = {
+		.mode = (I2S_MODE_MASTER | I2S_MODE_RX),
+		.sample_rate = 16000,
+		.bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+		.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+		.communication_format = (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB), // I2S_COMM_FORMAT_STAND_I2S,
+		.tx_desc_auto_clear = false,
+		.dma_buf_count = 4,
+		.dma_buf_len = 8};
+
+	i2s_pin_config_t pin_config = {
+		.mck_io_num = I2S_PIN_NO_CHANGE, .bck_io_num = I2S_BCK_IO, .ws_io_num = I2S_WS_IO, .data_out_num = I2S_DO_IO,
+		.data_in_num = I2S_DI_IO // Not used
+	};
+	i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+	i2s_set_pin(I2S_NUM, &pin_config);
+}
+
+#define N_SAMPLES 512
+#define xres 32 // Total number of  columns in the display
+#define yres 8	// Total number of  rows in the display
+
+int Displacement = 1; // Create LED Object
+
+int N = N_SAMPLES;
+int32_t sample[N_SAMPLES];
+float y_cf[N_SAMPLES];
+float sum_y[N_SAMPLES];
+int16_t freq_offset[16] = {0, 1, 2, 3, 4, 5, 8, 11, 17, 25, 30, 41, 44, 87, 175, 711};
+float tempraw[18];
+
+double vReal[N_SAMPLES]; // 实部 FFT采样输入样本数组
+double vImag[N_SAMPLES]; // 虚部 FFT运算输出数组
+double fft_bin[N_SAMPLES];
+double fft_data[32];
+int fft_result[32];
+uint8_t sens = 16;
+int freq_gain2[xres] = {30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+double fft_freq_boost[32] = {1.02,1.03,1.04,1.06,1.08,1.10,1.10,1.11,1.12,1.13,1.15,1.16,1.17,1.18,1.20,1.21,1.30,1.51,2.11,2.22,3.25,3.26,3.52,3.55,4.22,4.24,5.52,5.55,6.53,6.55,8.82,8.88};
+int gain = 30;			  // adjust it to set the gain
+int Intensity[xres] = {}; // initialize Frequency Intensity to zero
+unsigned long lastTime = 0;
+int fft_value_old[32];
+
+uint8_t bar_height[]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint8_t peak_height[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint8_t prev_fft_value[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+double fft_add(int from, int to)
+{
+	int i = from;
+	double result = 0;
+	while (i <= to)
+	{
+		result += fft_bin[i++];
 	}
+	return result;
 }
 
 int awtrix_init(void)
@@ -198,17 +263,143 @@ int awtrix_init(void)
 
 	awtrix_display_set_wifi((pixel_u *)&pixel);
 
+	awtrix_i2s_init();
+
 	if (awtrix_display_handle == NULL)
 	{
 		xTaskCreate(awtrix_display_task, "awtrix_display", 4096, NULL, 5, &awtrix_display_handle);
 	}
+
+	esp_err_t ret;
+	ESP_LOGI(TAG, "Start Example.");
+
+	while (1)
+	{
+		size_t i2s_bytes_read;
+		i2s_read(I2S_NUM_0, (char *)&sample, sizeof(int32_t) * N_SAMPLES, &i2s_bytes_read, portMAX_DELAY); // no timeout
+		if ((i2s_bytes_read > 0) && (i2s_bytes_read = N_SAMPLES * 4))
+		{
+
+			for (int i = 0; i < N_SAMPLES; i++)
+			{
+				sample[i] = abs(sample[i] >> sens);
+				vReal[i] = sample[i];
+				vImag[i] = 0;
+			}
+
+			bool reduce = false;
+			unsigned long millis = (unsigned long)(esp_timer_get_time() / 1000ULL);
+			if ((millis - lastTime) > 25)
+			{
+				lastTime = millis;
+				reduce = true;
+			}
+			// fft
+			fft_windowing(vReal, 1, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+			fft_compute(vReal, vImag, N_SAMPLES, FFT_FORWARD);
+			fft_complexToMagnitude(vReal, vImag, N_SAMPLES);
+			// Update Intensity Array
+
+			for (int i = 0; i < N_SAMPLES; i++)
+			{
+				double t = 0.0;
+				t = abs(vReal[i]);
+				t = t / 32.0;
+				fft_bin[i] = t;
+			}
+
+			fft_data[0] = (fft_add(3, 4)) / 2;		 // 60-100Hz
+			fft_data[1] = (fft_add(4, 5)) / 2;		 // 80-120Hz
+			fft_data[2] = (fft_add(5, 6)) / 2;		 // 100-140Hz
+			fft_data[3] = (fft_add(6, 7)) / 2;		 // 120-160Hz
+			fft_data[4] = (fft_add(7, 8)) / 2;		 // 140-180Hz
+			fft_data[5] = (fft_add(8, 9)) / 2;		 // 160-200Hz
+			fft_data[6] = (fft_add(9, 10)) / 2;		 // 180-220Hz
+			fft_data[7] = (fft_add(10, 11)) / 2;	 // 200-240Hz
+			fft_data[8] = (fft_add(11, 12)) / 2;	 // 220-260Hz
+			fft_data[9] = (fft_add(12, 13)) / 2;	 // 240-280Hz
+			fft_data[10] = (fft_add(13, 14)) / 2;	 // 260-300Hz
+			fft_data[11] = (fft_add(14, 16)) / 3;	 // 280-340Hz
+			fft_data[12] = (fft_add(16, 18)) / 3;	 // 320-380Hz
+			fft_data[13] = (fft_add(18, 20)) / 3;	 // 360-420Hz
+			fft_data[14] = (fft_add(20, 24)) / 5;	 // 400-500Hz
+			fft_data[15] = (fft_add(24, 28)) / 5;	 // 480-580Hz
+			fft_data[16] = (fft_add(28, 32)) / 5;	 // 560-660Hz
+			fft_data[17] = (fft_add(32, 36)) / 5;	 // 640-740Hz
+			fft_data[18] = (fft_add(36, 42)) / 7;	 // 720-860Hz
+			fft_data[19] = (fft_add(42, 48)) / 7;	 // 840-980Hz
+			fft_data[20] = (fft_add(48, 56)) / 9;	 // 960-1140Hz
+			fft_data[21] = (fft_add(56, 64)) / 9;	 // 1120-1300Hz
+			fft_data[22] = (fft_add(64, 74)) / 11;	 // 1280-1500Hz
+			fft_data[23] = (fft_add(74, 84)) / 11;	 // 1480-1700Hz
+			fft_data[24] = (fft_add(84, 97)) / 14;	 // 1680-1960Hz
+			fft_data[25] = (fft_add(97, 110)) / 14;	 // 1940-2240Hz
+			fft_data[26] = (fft_add(110, 128)) / 19; // 2200-2580Hz
+			fft_data[27] = (fft_add(128, 146)) / 19; // 2560-2940Hz
+			fft_data[28] = (fft_add(146, 170)) / 25; // 2920-3420Hz
+			fft_data[29] = (fft_add(170, 194)) / 25; // 3400-3900Hz
+			fft_data[30] = (fft_add(194, 224)) / 31; // 3880-4500Hz
+			fft_data[31] = (fft_add(224, 255)) / 32; // 4520-5120Hz
+
+			// adjust single frequency curves.
+			for (int i = 0; i < 32; i++)
+			{
+				fft_data[i] = fft_data[i] * fft_freq_boost[i];
+			}
+
+			// adjust overall frequency curves.
+			for (int i = 0; i < 32; i++)
+			{
+				fft_data[i] = fft_data[i] * gain / 50;
+			}
+
+			// constraint function
+			for (int i = 0; i < 32; i++)
+			{
+				fft_result[i] = constrain((int)fft_data[i], 0, 255);
+			}
+
+			for (int i = 0; i < 32; i = i + 1)
+			{
+				uint8_t fft_value;
+				fft_value = fft_result[i];
+				fft_value = ((prev_fft_value[i] * 3) + fft_value) / 4;
+				bar_height[i] = fft_value / (255 / (8 - 1)); // scale bar height
+				if (bar_height[i] > peak_height[i])						 // peak up
+					peak_height[i] = ((8-1) > ((int)bar_height[i]) ? (int)bar_height[i] : (8-1));
+				prev_fft_value[i] = fft_value;
+			}
+
+			// int t = 16;
+			// for (int i = t; i < (xres * Displacement) + t; i += Displacement)
+			// {
+			// 	vReal[i] = constrain(vReal[i], 0, 3596);									 // set max value for input data
+			// 	vReal[i] = map(vReal[i], freq_gain2[(i - t) / Displacement], 1848, 0, yres); // map data to fit our display
+			// 	if (reduce)
+			// 	{
+			// 		Intensity[(i - t) / Displacement]--; // Decrease displayed value
+			// 	}
+			// 	if (vReal[i] > Intensity[(i - t) / Displacement]) // Match displayed value to measured value
+			// 		Intensity[(i - t) / Displacement] = vReal[i];
+			// }
+			for (int i = 0; i < 32; i++)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					if ((j <= bar_height[i]))
+						pixel[7 - j][i].g = 0x10;
+					else
+						pixel[7 - j][i].rgb = 0;
+				}
+			}
+			// printf("%d ", fft_result[i]);
+			// printf("\r\n");
+		}
+	}
+
 	if (awtrix_status_handle == NULL)
 	{
 		xTaskCreate(awtrix_status_task, "awtrix_status", 4096, NULL, 5, &awtrix_status_handle);
-	}
-	if (awtrix_ferquency_handle == NULL)
-	{
-		xTaskCreate(awtrix_ferquency_task, "awtrix_ferquency", 4096, NULL, 5, &awtrix_ferquency_handle);
 	}
 
 	time(&now);
--- project/drivers/fft/include/fft.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FFT__
-#define __FFT__
-
-void kfft(double *pr, double *pi, int n, int k, double *fr, double *fi);
-
-#endif
